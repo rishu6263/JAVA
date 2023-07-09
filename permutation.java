81d942812ec8b24d6f76e879e116d219d3ddda51
RISHABH DWIVEDI <107019555+rishu6263@users.noreply.github.com>
2023-07-09 11:03:04 +0530

    Add files via upload

---
+++ permutation.java
@@ -0,0 +1,17 @@
+public class permutation {
+    public static void main(String[] args) {
+        Permutation("","abc");
+    }
+    static void Permutation(String p,String up){
+        if(up.isEmpty()){
+            System.out.println(p);
+            return;
+        }
+        char ch=up.charAt(0);
+        for(int i=0;i<=p.length();i++) {
+            String f = p.substring(0, i);
+            String sc=p.substring(i,p.length());
+            Permutation(f+ch+sc,up.substring(1));
+        }
+    }
+}
