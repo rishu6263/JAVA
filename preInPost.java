7f64235e290f7828e0780d3506c69e446c1b7593
RISHABH DWIVEDI <107019555+rishu6263@users.noreply.github.com>
2023-07-13 22:50:24 +0530

    deleted preInPost.java

--- preInPost.java
+++ preInPost.java
@@ -1,14 +1,1 @@
-public class preInPost {
-    public static void main(String[] args) {
-        pip(3);
-    }
 
-    private static void pip(int i) {
-        if(i==0) return;
-        System.out.println(i);
-        pip(i-1);
-        System.out.println(i);
-        pip(i-1);
-        System.out.println(i);
-    }
-}
